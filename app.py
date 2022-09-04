ca45a34a2eec2686377530920d04ebc8a8adb30e
Mark Weng <tpl86048381@gmail.com>
2022-09-04 17:55:34 +0800

    fix

--- app.py
+++ app.py
@@ -54,6 +54,9 @@ def handle_message(event):
         r = 'hi'
     elif msg == '你吃飯了嗎':
         r == '還沒'
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=r))
 
 
     
